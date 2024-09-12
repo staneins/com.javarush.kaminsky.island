aae5c5ea48471cdc0053d346dc670b38f76b3d5f
Philip Kaminsky <kaminsky8836@gmail.com>
2024-09-12 09:28:26 +0300

    initial commit

---
+++ src/main/java/com/javarush/kaminsky/App.java
@@ -0,0 +1,13 @@
+package com.javarush.kaminsky;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
